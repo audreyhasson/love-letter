dde50e5a921c7070b62719bbc23852b8e49e05d0
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2024-07-14 10:04:05 -0400

    feat: created template and page split

---
+++ love-letter/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex h-[100vh] w-[100vw]">
+      <div className="bg-pink-200 w-1/2">
+
+      </div>
+      <div className="bg-indigo-200 w-1/2">
+
+      </div>
+    </main>
+  );
+}
