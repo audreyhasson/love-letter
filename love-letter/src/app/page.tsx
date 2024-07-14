a207e255f7f258a42168f67ac1b9c81bdb99dc1d
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2024-07-14 10:12:10 -0400

    feat: added skeleton to home page and fake data

--- love-letter/src/app/page.tsx
+++ love-letter/src/app/page.tsx
@@ -1,13 +1,21 @@
-import Image from "next/image";
+import { posts } from "@/app/fake-posts.json"
 
 export default function Home() {
+
+  const audsPosts = posts.filter((item) => item.author == "auds");
+  const kevPosts = posts.filter((item) => item.author == "kev"); 
+
+  console.log(audsPosts, kevPosts);
+
   return (
     <main className="flex h-[100vh] w-[100vw]">
       <div className="bg-pink-200 w-1/2">
-
+          <PostsView posts={audsPosts}/>
+          <NewPostButton author="auds" />
       </div>
       <div className="bg-indigo-200 w-1/2">
-
+          <PostsView posts={kevPosts}/>
+          <NewPostButton author="kev"/>
       </div>
     </main>
   );
